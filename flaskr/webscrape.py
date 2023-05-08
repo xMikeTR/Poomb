3c8dade5394bdd22844df1d17a6b9cef3b130598
Mike <miketr1990@gmail.com>
2023-05-08 19:00:20 +0100

    installed parsers, scraped them

--- flaskr/webscrape.py
+++ flaskr/webscrape.py
@@ -1,18 +1,12 @@
 from flask import Blueprint
 from flask_restx import Api, Resource
-#from flaskr.db import get_db
-from datetime import datetime
-from instascrape import *
+from bs4 import BeautifulSoup
+import requests
+
+
 
 bp = Blueprint('api', __name__)
 wtlfcal = Api(bp)
 
 # use pypi insta-scraper and facebook scraper to look through wpc posts and grab data
 #seek keywords and print it in Json, display through AJAX
-google = Profile('https://www.instagram.com/wpcportugal/?utm_source=ig_embed&hl=en')
-google_post = Post('https://www.instagram.com/wpcportugal/?hl=en')
-
-google.scrape()
-google_post.scrape()
-
-print(google_post['hashtags'])
