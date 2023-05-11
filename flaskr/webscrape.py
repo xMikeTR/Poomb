2749e372d6d7c721672a535327db41c2e38d9f1b
Mike <miketr1990@gmail.com>
2023-05-11 19:32:55 +0100

    added files to webscrape

--- flaskr/webscrape.py
+++ flaskr/webscrape.py
@@ -4,24 +4,37 @@ from bs4 import BeautifulSoup
 import nest_asyncio
 import asyncio
 from pyppeteer import launch
+from PIL import Image
+import pytesseract
 nest_asyncio.apply()
 
 
 
-async def main():
+async def extract_image_text(url):
     browser = await launch(headless=True)
     page = await browser.newPage()
-    post_url = 'https://www.instagram.com/p/COmlv7iLHlG/'
 
-    await page.goto(post_url,)
-    await page.waitForSelector('img[alt]')
+    await page.goto(url)
+    await page.waitForSelector('.wp-image-5559')
 
-    html = await page.content()
-    soup = BeautifulSoup(html, 'html.parser')
-    img_tag = soup.find('img')
-    post_text = img_tag['alt']
+    image_data = await page.evaluate('(async () => {'
+                                    '  const image = document.querySelector(".wp-image-5559");'
+                                    '  const canvas = document.createElement("canvas");'
+                                    '  const context = canvas.getContext("2d");'
+                                    '  canvas.width = image.width;'
+                                    '  canvas.height = image.height;'
+                                    '  context.drawImage(image, 0, 0, image.width, image.height);'
+                                    '  return canvas.toDataURL();'
+                                    '})()')
 
+    await browser.close()
 
+    image = Image.open(BytesIO(base64.b64decode(image_data.split(',')[1])))
+    text = pytesseract.image_to_string(image)
 
-response = asyncio.get_event_loop().run_until_complete(main())
-print(response)
+    return text
+
+website_url = 'https://powerexpoportugal.com/'
+image_text = asyncio.get_event_loop().run_until_complete(extract_image_text(website_url))
+
+print('Image Text:', image_text)
