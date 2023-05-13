3e972786400eae1c7e2d4b24986dbd8d6181cacc
Mike <miketr1990@gmail.com>
2023-05-13 12:10:04 +0100

    removed tesseract related libs

--- flaskr/webscrape.py
+++ flaskr/webscrape.py
@@ -2,46 +2,22 @@ import asyncio
 import os
 from pyppeteer import launch
 from bs4 import BeautifulSoup
-from PIL import Image
-import pytesseract
-import base64
-from io import BytesIO
 import nest_asyncio
 
 nest_asyncio.apply()
 
-async def extract_image_text(url):
-    browser = await launch(headless=True, timeout=0)
+async def get_page_text(url):
+    browser = await launch()
     page = await browser.newPage()
-
     await page.goto(url)
-    await page.waitForSelector('.wp-image-5559')
-
-    image_data = await page.evaluate('() => {'
-                                    '  const image = document.querySelector(".wp-image-5559");'
-                                    '  const canvas = document.createElement("canvas");'
-                                    '  const context = canvas.getContext("2d");'
-                                    '  canvas.width = image.width;'
-                                    '  canvas.height = image.height;'
-                                    '  context.drawImage(image, 0, 0, image.width, image.height);'
-                                    '  return canvas.toDataURL();'
-                                    '}')
-
+    await page.evaluate('window.scrollTo(0, document.body.scrollHeight)')
+    await asyncio.sleep(3) # Wait for page to load completely
+    html = await page.content()
+    soup = BeautifulSoup(html, 'html.parser')
+    page_text = soup.get_text()
     await browser.close()
+    return page_text
 
-    tessdata_dir = r'C:/Program Files/Tesseract-OCR/tessdata'
-    tessdata_dir_config = f'--tessdata-dir {tessdata_dir}'
-    tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
-    pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
-
-    os.environ['TESSDATA_PREFIX'] = tessdata_dir
-
-    image = Image.open(BytesIO(base64.b64decode(image_data.split(',')[1])))
-    text = pytesseract.image_to_string(image, lang='por', config=tessdata_dir_config)
-
-    return text
-
-website_url = 'https://powerexpoportugal.com/'
-image_text = asyncio.get_event_loop().run_until_complete(extract_image_text(website_url))
-
-print('Image Text:', image_text)
+url = 'https://powerexpoportugal.com/'
+page_text = asyncio.get_event_loop().run_until_complete(get_page_text(url))
+print(page_text)
