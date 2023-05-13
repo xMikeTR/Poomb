6cd6f356fd07f5e4602b17a831049254a2b3dcd0
Mike <miketr1990@gmail.com>
2023-05-13 11:15:28 +0100

    1

--- flaskr/webscrape.py
+++ flaskr/webscrape.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from pyppeteer import launch
 from bs4 import BeautifulSoup
 from PIL import Image
@@ -6,10 +7,11 @@ import pytesseract
 import base64
 from io import BytesIO
 import nest_asyncio
-nest_asyncio.apply() 
+
+nest_asyncio.apply()
 
 async def extract_image_text(url):
-    browser = await launch(headless=True)
+    browser = await launch(headless=True, timeout=0)
     page = await browser.newPage()
 
     await page.goto(url)
@@ -26,13 +28,16 @@ async def extract_image_text(url):
                                     '}')
 
     await browser.close()
-    
-    tessdata_dir_config = '--tessdata-dir "<C:\Program Files\Tesseract-OCR\tessdata>"'
-    tesseract_cmd = r'<C:\Program Files\Tesseract-OCR\tesseract.exe>'
+
+    tessdata_dir = r'C:/Program Files/Tesseract-OCR/tessdata'
+    tessdata_dir_config = f'--tessdata-dir {tessdata_dir}'
+    tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
     pytesseract.pytesseract.tesseract_cmd = tesseract_cmd
 
+    os.environ['TESSDATA_PREFIX'] = tessdata_dir
+
     image = Image.open(BytesIO(base64.b64decode(image_data.split(',')[1])))
-    text = pytesseract.image_to_string(image, config=tessdata_dir_config)
+    text = pytesseract.image_to_string(image, lang='por', config=tessdata_dir_config)
 
     return text
 
